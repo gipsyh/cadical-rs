2f13f0741c01f6fb1fe5ccfb5d9e28f93bd0d495
Yuheng Su <gipsyh.icu@gmail.com>
2024-03-03 10:44:39 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 use logic_form::{Lit, Var};
-use std::{
-    ffi::{c_int, c_void},
-    fmt::{self, Debug},
-    marker::PhantomData,
-};
+use satif::{SatResult, Satif, SatifConflict, SatifModel};
+use std::ffi::{c_int, c_void};
 
 extern "C" {
     fn cadical_solver_new() -> *mut c_void;
     fn cadical_solver_free(s: *mut c_void);
     fn cadical_solver_add_clause(s: *mut c_void, clause: *mut c_int, len: c_int);
     fn cadical_solver_solve(s: *mut c_void, assumps: *mut c_int, len: c_int) -> c_int;
-    fn cadical_solver_constrain(s: *mut c_void, constrain: *mut c_int, len: c_int);
-    fn cadical_solver_simplify(s: *mut c_void);
+    // fn cadical_solver_constrain(s: *mut c_void, constrain: *mut c_int, len: c_int);
+    // fn cadical_solver_simplify(s: *mut c_void);
     // fn solver_set_polarity(s: *mut c_void, var: c_int, pol: c_int);
     fn cadical_solver_model_value(s: *mut c_void, lit: c_int) -> c_int;
     fn cadical_solver_conflict_has(s: *mut c_void, lit: c_int) -> bool;
@@ -25,13 +22,12 @@ fn lit_to_cadical_lit(lit: &Lit) -> i32 {
     res
 }
 
-pub struct Model<'a> {
+pub struct Model {
     solver: *mut c_void,
-    _pd: PhantomData<&'a ()>,
 }
 
-impl Model<'_> {
-    pub fn lit_value(&self, lit: Lit) -> Option<bool> {
+impl SatifModel for Model {
+    fn lit_value(&self, lit: Lit) -> Option<bool> {
         let lit = lit_to_cadical_lit(&lit);
         let res = unsafe { cadical_solver_model_value(self.solver, lit) };
         if res == lit {
@@ -44,29 +40,14 @@ impl Model<'_> {
     }
 }
 
-pub struct Conflict<'a> {
+pub struct Conflict {
     solver: *mut c_void,
-    _pd: PhantomData<&'a ()>,
 }
 
-impl Conflict<'_> {
-    pub fn has(&self, lit: Lit) -> bool {
+impl SatifConflict for Conflict {
+    fn has(&self, lit: Lit) -> bool {
         let lit = lit_to_cadical_lit(&lit);
         unsafe { cadical_solver_conflict_has(self.solver, lit) }
-    }
-}
-
-pub enum SatResult<'a> {
-    Sat(Model<'a>),
-    Unsat(Conflict<'a>),
-}
-
-impl Debug for SatResult<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Sat(_) => "Sat".fmt(f),
-            Self::Unsat(_) => "Unsat".fmt(f),
-        }
     }
 }
 
@@ -75,60 +56,68 @@ pub struct Solver {
     num_var: usize,
 }
 
-impl Solver {
-    pub fn new() -> Self {
+impl Satif for Solver {
+    type Model = Model;
+
+    type Conflict = Conflict;
+
+    #[inline]
+    fn new() -> Self {
         Self {
             solver: unsafe { cadical_solver_new() },
             num_var: 0,
         }
     }
 
-    pub fn new_var(&mut self) -> Var {
+    #[inline]
+    fn new_var(&mut self) -> Var {
         self.num_var += 1;
         Var::new(self.num_var - 1)
     }
 
-    pub fn num_var(&self) -> usize {
+    #[inline]
+    fn num_var(&self) -> usize {
         self.num_var
     }
 
-    pub fn add_clause(&mut self, clause: &[Lit]) {
+    #[inline]
+    fn add_clause(&mut self, clause: &[Lit]) {
         let clause: Vec<i32> = clause.iter().map(lit_to_cadical_lit).collect();
         unsafe { cadical_solver_add_clause(self.solver, clause.as_ptr() as _, clause.len() as _) }
     }
 
-    pub fn solve<'a>(&'a mut self, assumps: &[Lit]) -> SatResult<'a> {
+    fn solve(&mut self, assumps: &[Lit]) -> SatResult<Self::Model, Self::Conflict> {
         let assumps: Vec<i32> = assumps.iter().map(lit_to_cadical_lit).collect();
         match unsafe {
             cadical_solver_solve(self.solver, assumps.as_ptr() as _, assumps.len() as _)
         } {
             10 => SatResult::Sat(Model {
                 solver: self.solver,
-                _pd: PhantomData,
             }),
             20 => SatResult::Unsat(Conflict {
                 solver: self.solver,
-                _pd: PhantomData,
             }),
             _ => todo!(),
         }
     }
+}
 
-    pub fn solve_with_constrain<'a>(
-        &'a mut self,
-        assumps: &[Lit],
-        constrain: &[Lit],
-    ) -> SatResult<'a> {
-        let constrain: Vec<i32> = constrain.iter().map(lit_to_cadical_lit).collect();
-        unsafe {
-            cadical_solver_constrain(self.solver, constrain.as_ptr() as _, constrain.len() as _)
-        };
-        self.solve(assumps)
-    }
+impl Solver {
+    // pub fn solve_with_constrain<'a>(
+    //     &'a mut self,
+    //     assumps: &[Lit],
+    //     constrain: &[Lit],
+    // ) -> SatResult<'a> {
+    //     let constrain: Vec<i32> = constrain.iter().map(lit_to_cadical_lit).collect();
+    //     unsafe {
+    //         cadical_solver_constrain(self.solver, constrain.as_ptr() as _, constrain.len() as _)
+    //     };
+    //     self.solve(assumps)
+    // }
 
-    pub fn simplify(&mut self) {
-        unsafe { cadical_solver_simplify(self.solver) };
-    }
+    // pub fn simplify(&mut self) {
+    //     unsafe { cadical_solver_simplify(self.solver) };
+    // }
 
     // pub fn set_polarity(&mut self, var: Var, pol: Option<bool>) {
     //     let pol = match pol {
@@ -149,19 +138,17 @@ impl Solver {
 
     /// # Safety
     /// unsafe get sat model
-    pub unsafe fn get_model(&self) -> Model<'static> {
+    pub unsafe fn get_model(&self) -> Model {
         Model {
             solver: self.solver,
-            _pd: PhantomData,
         }
     }
 
     /// # Safety
     /// unsafe get unsat core
-    pub unsafe fn get_conflict(&self) -> Conflict<'static> {
+    pub unsafe fn get_conflict(&self) -> Conflict {
         Conflict {
             solver: self.solver,
-            _pd: PhantomData,
         }
     }
 }
@@ -196,15 +183,15 @@ fn test() {
         }
         SatResult::Unsat(_) => todo!(),
     }
-    solver.simplify();
-    match solver.solve_with_constrain(&[lit2], &[!lit0]) {
-        SatResult::Sat(_) => panic!(),
-        SatResult::Unsat(conflict) => {
-            assert!(conflict.has(lit2));
-        }
-    }
-    match solver.solve(&[lit2]) {
-        SatResult::Sat(_) => {}
-        SatResult::Unsat(_) => todo!(),
-    }
+    // solver.simplify();
+    // match solver.solve_with_constrain(&[lit2], &[!lit0]) {
+    //     SatResult::Sat(_) => panic!(),
+    //     SatResult::Unsat(conflict) => {
+    //         assert!(conflict.has(lit2));
+    //     }
+    // }
+    // match solver.solve(&[lit2]) {
+    //     SatResult::Sat(_) => {}
+    //     SatResult::Unsat(_) => todo!(),
+    // }
 }
