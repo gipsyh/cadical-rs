829a1cb388ede36f865a20d0d6590840f9a59b9e
Yuheng Su <gipsyh.icu@gmail.com>
2024-12-16 10:48:53 +0000

    v0.1.2

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 #![feature(exit_status_error)]
 use cmake::Config;
+use giputils::build::copy_build;
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 
@@ -10,7 +10,6 @@ fn main() -> Result<(), String> {
         .args(["submodule", "update", "--init"])
         .status()
         .unwrap();
-    let src_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
 
     let bindings = PathBuf::from("./bindings");
@@ -18,31 +17,26 @@ fn main() -> Result<(), String> {
     Config::new(bindings).build();
 
     println!("cargo:rerun-if-changed=./cadical");
-    let cadical_src = PathBuf::from(src_dir).join("cadical");
-    let cadical_out = PathBuf::from(&out_dir).join("cadical");
-    let num_jobs = env::var("NUM_JOBS").unwrap();
-    if cadical_out.exists() {
-        fs::remove_dir_all(&cadical_out).unwrap();
-    }
-    fs::create_dir(&cadical_out).unwrap();
-    let overlay = giputils::mount::MountOverlay::new(&cadical_src, &cadical_out);
-    Command::new("sh")
-        .arg("configure")
-        .current_dir(overlay.path())
-        .status()
-        .map_err(|e| e.to_string())?
-        .exit_ok()
-        .map_err(|e| e.to_string())?;
-    Command::new("make")
-        .arg(format!("-j{}", num_jobs))
-        .current_dir(overlay.path())
-        .status()
-        .map_err(|e| e.to_string())?
-        .exit_ok()
-        .map_err(|e| e.to_string())?;
+    let cb_path = copy_build("cadical", |src| {
+        Command::new("sh")
+            .arg("configure")
+            .current_dir(src)
+            .status()
+            .map_err(|e| e.to_string())?
+            .exit_ok()
+            .map_err(|e| e.to_string())?;
+        let num_jobs = env::var("NUM_JOBS").unwrap();
+        Command::new("make")
+            .arg(format!("-j{}", num_jobs))
+            .current_dir(src)
+            .status()
+            .map_err(|e| e.to_string())?
+            .exit_ok()
+            .map_err(|e| e.to_string())
+    })?;
     println!(
         "cargo:rustc-link-search=native={}",
-        cadical_out.join("build").display()
+        cb_path.join("build").display()
     );
     println!(
         "cargo:rustc-link-search=native={}",
